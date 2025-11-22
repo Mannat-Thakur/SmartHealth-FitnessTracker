6a4c49a4c9fde889740e23d2922c03e2a72e8d8f
Mannat-Thakur <mannatthakur2512@gmail.com>
2025-11-22 21:08:36 +0530

    Implement application entry point

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "FitnessTracker.h" // Include your main engine
+
+int main()
+{
+    // Create object
+    FitnessTracker app;
+
+    // main 'run' method
+    app.run();
+
+    return 0;
+}
